e30f279daa9c009fd4fb3af3fbe0102bdb6a0e71
Celso Godinho da Costa <celsog@edu.pbh.gov.br>
2024-06-26 13:00:42 -0300

    Ajuste largura do grafico

--- paginas/regional.py
+++ paginas/regional.py
@@ -144,7 +144,7 @@ def regional(nome_regional, regional_id, dias_letivos, ano, mes):
               dict(name='Entre 20% e 25%',data=resultado['p_20_25'].tolist()),
               dict(name='Maior ou igual à 25%',data=resultado['p_25_100'].tolist())]
 
-    st_apexcharts(options, series, 'bar', 1600)    
+    st_apexcharts(options, series, 'bar', 1500)    
 
     rename = {
         'nome':'Local', 
